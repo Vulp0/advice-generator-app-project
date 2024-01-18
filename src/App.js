5f7b9fff5f2fd5a55fc896234a71c0da3e8d2094
Mau <mauriciomartinezvazquez@rocketmail.com>
2024-01-18 17:19:13 -0600

    works, with some details

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import Dice from './icon-dice.svg';
 import Separator from './pattern-divider-mobile.svg';
 
 function App() {
+    async function getAdvice() {
+      const adviceObj = (await (await fetch("https://api.adviceslip.com/advice")).json()).slip;
+      return adviceObj;
+    }
+
+    async function updateText() {
+      let advice = await getAdvice();
+      const AdviceNumberElement = document.querySelector(".AdviceNumber");
+      const AdviceTextElement = document.querySelector(".AdviceText");
+
+      AdviceNumberElement.textContent = "Advice #" + advice.id;
+      AdviceTextElement.textContent = `"${advice.advice}"`;
+    }
+    
   return (
     <div className="App">
       <main className='MainContainer'>
-        <header className='AdviceNumber'>advice #69</header>
-        <main className='AdviceText'>"It is easy to sit up and take notice, what’s difficult is getting up and taking action"</main>
+        <header className='AdviceNumber'>advice #{69}</header>
+        <main className='AdviceText'>"{"It is easy to sit up and take notice, what’s difficult is getting up and taking action"}"</main>
         <img className='Separator' src={Separator} alt=''></img>
-        <button className='NewAdviceButton'>
+        <button className='NewAdviceButton' onClick={updateText}>
           <img src={Dice} alt=''></img>
         </button>
       </main>
+      <script src='./magic.js'></script>
     </div>
   );
 }
